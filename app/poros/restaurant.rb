cab05d6272bab3856cb6ac201027c192b064a13d
Micha Bernhard <mib70@pitt.edu>
2022-01-07 16:00:03 -0700

    change distance to use mapquest

--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -1,11 +1,11 @@
 class Restaurant
-  attr_reader :name, :display_phone, :rating, :display_address, :distance
+  attr_accessor :name, :display_phone, :rating, :display_address, :distance
 
   def initialize(restaurant_data)
     @name = restaurant_data[:name]
     @display_phone = restaurant_data[:display_phone]
     @rating = restaurant_data[:rating]
     @display_address = restaurant_data[:location][:display_address].join(', ')
-    @distance = restaurant_data[:distance]
+    @distance = 0
   end
 end
