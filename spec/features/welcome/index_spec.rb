fff8d7cf5d1452bfaa641e924cd71de9ffad2a1d
Chaz Simons <chaz.simons@gmail.com>
2022-01-07 13:33:57 -0800

    A familiar error

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe 'Root Page' do
     expect(page).to have_field(:craving)
     expect(page).to have_button("Search For Restaurants")
   end
+
+  it 'allows a user to enter their city and craving' do
+    visit '/'
+
+    fill_in :city, with: 'Las Vegas'
+    fill_in :craving, with: 'fries'
+    click_button "Search For Restaurants"
+
+    expect(current_path).to be('/search')
+  end
 end
