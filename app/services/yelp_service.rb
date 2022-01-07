04ae6f9e520e63171029db302676ff1fb15434e7
Micha Bernhard <mib70@pitt.edu>
2022-01-07 15:27:54 -0700

    Hide yelp api key

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,6 +1,6 @@
 class YelpService
   def self.connection(endpoint)
-    conn = Faraday.new(url: "https://api.yelp.com", headers: { Authorization: 'Bearer 29328aTBMjyyJLTbR3_pKLuoGHGVZIqDtu-Eu0FI2a7o35XfBntIUNEMuiE5Vgzy-Sopn8qFri1TPw3_PKOKZJTqlJB63XDeXGXONSHr47xD-LXu3kUFZx7tdLLYYXYx' })
+    conn = Faraday.new(url: "https://api.yelp.com", headers: { Authorization: ENV['yelp_key'] })
     response = conn.get(endpoint)
 
     data = JSON.parse(response.body, symbolize_names: true)
