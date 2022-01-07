db49c5c9434d87b1e1cc2b1665fe55a97a4f45c6
Sam Devine <samuel.will.devine@gmail.com>
2022-01-07 14:31:15 -0700

    Test Service - add yelp api key to application.yml

--- app/services/restaurants_service.rb
+++ app/services/restaurants_service.rb
@@ -1,5 +1,18 @@
 class RestaurantsService
-  def self.conn
-    Faraday.new
+  class << self
+    def conn
+      Faraday.new('https://api.yelp.com/v3/')
+    end
+
+    def parse(url, params)
+      headers = { 'Authorization': "Bearer #{ENV['YELP_API_KEY']}" }
+      response = conn.get(url, params, headers)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def restaurants_search(location, term)
+      params= { location: location, term: term }
+      parse('businesses/search', params)
+    end
   end
 end
--- spec/services/restaurants_service_spec.rb
+++ spec/services/restaurants_service_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe RestaurantsService do
   it 'creates a faraday connection' do
     expect(RestaurantsService.conn).to be_a Faraday::Connection
   end
+
+  it 'connects to the yelp api' do
+    location = 'Denver'
+    term = 'pizza'
+    results = RestaurantsService.restaurants_search(location, term)
+
+    expect(results).to be_a Hash
+    expect(results).to have_key :businesses
+    expect(results[:businesses][0]).to have_key :name
+    expect(results[:businesses][0][:name]).to eq "Brooklyn's Finest Pizza - Lowell"
+  end
 end
