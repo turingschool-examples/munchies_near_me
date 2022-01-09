26ede846cc6722da94d3ea2d62462bf3f4234d39
Stephanie Helm <stephaniemhelm@gmail.com>
2022-01-08 20:04:51 -0700

    feat test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,6 @@ class SearchController < ApplicationController
     craving = params[:craving]
 
     @restaurant_list = YelpFacade.get_search_restaurant_data(city, craving)
-    #require "pry"; binding.pry
     #render json: restaurant_list
   end
 end
--- spec/facades/yelp_facade_spec.rb
+++ spec/facades/yelp_facade_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe YelpService do
+RSpec.describe YelpFacade do
   it 'can get restaurant data' do
     city = "Denver, CO"
     craving = "coffee"
 
-    result = YelpService.get_search_restaurant_data(city, craving)
+    data = YelpFacade.get_search_restaurant_data(city, craving)
+
+    result = data.first
+    expect(result). to be_a Restaurant
+    expect(result.name).to eq("Thump Coffee")
+    expect(result.phone_number).to eq("+17204566648")
+    expect(result.distance).to eq(0.752)
+    expect(result.address).to eq(["1201 E 13th Ave", "Denver, CO 80218"])
+    expect(result.rating).to eq(4.0)
+  end
+end
