2df2d8177f885d316eec24145d6149905815d5a4
Christina Delpone <cdelpone@gmail.com>
2022-01-10 17:25:09 -0700

    test facade methods

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 # ber spec/facades/search_facade_spec.rb
 RSpec.describe 'Search Facade' do
+
   it 'returns data on businesses based on location and category' do
     location = "denver, co"
     category = "thai"
@@ -31,6 +32,46 @@ RSpec.describe 'Search Facade' do
     category = "thai"
 
     restaurant = SearchFacade.restaurants(location, category)
+
     expect(restaurant.first).to be_a Restaurant
+  end
+
+  it 'returns restaurants closest to farthest' do
+    location = "denver, co"
+    category = "thai"
+
+    restaurant = SearchFacade.restaurants(location, category)
+
+    first = restaurant.first.distance
+    last = restaurant.last.distance
+
+    expected = first <= last
+
+    expect(expected).to be true
+  end
+
+  it 'returns a hash' do
+    location = "denver, co"
+    category = "thai"
+
+    all_restaurants = SearchFacade.find_restaurants_via_yelp_mq(location, category)
+    
+    expect(all_restaurants).to be_an Array
+    expect(all_restaurants.first).to be_a Hash
+
+    expect(all_restaurants.first).to have_key[:name]
+    expect(all_restaurants.first[:name]).to be_a String
+
+    expect(all_restaurants.first).to have_key[:rating]
+    expect(all_restaurants.first[:rating]).to be_a Float
+
+    expect(all_restaurants.first).to have_key[:display_phone]
+    expect(all_restaurants.first[:display_phone]).to be_a String
+
+    expect(all_restaurants.first).to have_key[:display_address]
+    expect(all_restaurants.first[:display_address]).to be_an Array
+
+    expect(all_restaurants.first).to have_key[:mq_distance]
+    expect(all_restaurants.first[:mq_distance]).to be_a Float
   end
 end
